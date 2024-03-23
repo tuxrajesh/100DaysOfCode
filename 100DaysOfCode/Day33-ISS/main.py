7804a3c96100792614a3c0a6873a47e0473110af
Rajeshwaran SP <tuxrajesh@gmail.com>
2024-03-22 21:25:05 -0700

    Day33: ISS

--- 100DaysOfCode/Day33-ISS/main.py
+++ 100DaysOfCode/Day33-ISS/main.py
@@ -4,32 +4,38 @@ from datetime import datetime
 MY_LAT = 37.70 # Your latitude
 MY_LONG = -121.9 # Your longitude
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
-data = response.json()
+def is_iss_overhead():
+    response = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response.raise_for_status()
+    data = response.json()
 
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
+    
+    #Your position is within +5 or -5 degrees of the ISS position.
+    if MY_LAT - 5 <= iss_latitude <= MY_LAT + 5.0 and MY_LONG - 5 <= iss_longitude <= MY_LONG + 5.0:
+        return True
 
-#Your position is within +5 or -5 degrees of the ISS position.
-parameters = {
-    "lat": MY_LAT,
-    "lng": MY_LONG,
-    "formatted": 0,
-}
+def is_night():
+    parameters = {
+        "lat": MY_LAT,
+        "lng": MY_LONG,
+        "tzid":"America/Los_Angeles",
+        "formatted": 0,
+    }
 
-response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response.raise_for_status()
-data = response.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
+    response = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response.raise_for_status()
+    data = response.json()
+    sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
 
-time_now = datetime.now()
+    time_now = datetime.now()
+    
+    if time_now > sunset or time_now <= sunrise:
+        return True
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+
 
 
 
